840674d6e5385495648eeba70bdf70a2e73d7891
Donn Pebe <donny.prasetyobudi@gmail.com>
2013-03-08 15:35:48 +0700

    use maximal 80 column in text editor

--- menuapp.rb
+++ menuapp.rb
@@ -9,30 +9,32 @@ def baca_xml
   puts my_file.inspect
   #myfile = 'resume.xml'
   xml=Nokogiri::XML File.open(my_file)
-  xml.xpath('//resource').each { |node| puts " - #{ node['name'] } \n ( #{ node['version'] } )" }
+  xml.xpath('//resource').each do |node| 
+    puts " - #{ node['name'] } \n ( #{ node['version'] } )" 
+  end
   puts " \n"
   puts " \n"
 end
 
 #method baca rss
 def baca_rss
- puts "masukkan url dari rss feed : "
- my_url = gets.gsub(/[\n\s]/,'')
- puts my_url.inspect
- 
- #contoh url rss feed
- #url = 'http://www.ruby-lang.org/en/feeds/news.rss'
- xml_file = open my_url
- doc = Nokogiri::XML xml_file
- doc.search('item').each do |item|
-  puts "========================================================================="
-     puts "Item: #{item.at('title').text}"
-   puts "Date: #{item.at('pubDate').text}"
-   puts "Url: #{item.at('link').text}"
-   puts "========================================================================="
-   puts " \n"
-   puts " \n"
- end
+  puts "masukkan url dari rss feed : "
+  my_url = gets.gsub(/[\n\s]/,'')
+  puts my_url.inspect
+
+  #contoh url rss feed
+  #url = 'http://www.ruby-lang.org/en/feeds/news.rss'
+  xml_file = open my_url
+  doc = Nokogiri::XML xml_file
+  doc.search('item').each do |item|
+    puts "===================================================================="
+    puts "Item: #{item.at('title').text}"
+    puts "Date: #{item.at('pubDate').text}"
+    puts "Url: #{item.at('link').text}"
+    puts "===================================================================="
+    puts " \n"
+    puts " \n"
+  end
 end
 
 
