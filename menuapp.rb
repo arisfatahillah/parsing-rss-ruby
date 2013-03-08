46808ef7143c180539a1f82505ef30e722fd0cfe
Donn Pebe <donny.prasetyobudi@gmail.com>
2013-03-08 15:09:51 +0700

    use method chaining

--- menuapp.rb
+++ menuapp.rb
@@ -6,12 +6,10 @@ require 'open-uri'
 #method baca xml
 def baca_xml
   puts "masukkan nama file xml : "
-  my_file = gets
-  myfile2 = my_file.gsub(/\n/," ")
-  myfile3 = myfile2.gsub(/\s/," ")
-  puts myfile3.inspect
+  my_file = gets.gsub(/[\n\s]/,'')
+  puts my_file.inspect
   #myfile = 'resume.xml'
-  xml=Nokogiri::XML File.open(myfile3)
+  xml=Nokogiri::XML File.open(my_file)
   xml.xpath('//resource').each { |node| puts " - #{ node['name'] } \n ( #{ node['version'] } )" }
   puts " \n"
   puts " \n"
@@ -20,15 +18,13 @@ end
 #method baca rss
 def baca_rss
  puts "masukkan url dari rss feed : "
- my_url = gets
- my_url2 = my_url.gsub(/\n/," ")
- my_url3 = my_url2.gsub(/\s/," ")
- puts my_url3.inspect
+ my_url = gets.gsub(/[\n\s]/,'')
+ puts my_url.inspect
  
  #contoh url rss feed
  #url = 'http://www.ruby-lang.org/en/feeds/news.rss'
  
- open(my_url3) do |rss|
+ open(my_url) do |rss|
    feed = RSS::Parser.parse(rss)
    puts "Title: #{feed.channel.title}"
    feed.items.each do |item|
