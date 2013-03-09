76d7896275b797285ff79702ca5c004949faaf89
arisfatahillah <arisfatahillah@gmail.com>
2013-03-09 22:46:38 +0700

    menyederhanakan program dengan menghapus satu method dan mengganti dengan if-else

--- menuapp.rb
+++ menuapp.rb
@@ -3,33 +3,25 @@ require 'nokogiri'
 require 'open-uri'
 
 #method baca xml
-def baca_xml
+def baca_xml(pilihan)
   puts "masukkan nama file xml : "
   my_file = gets.gsub(/[\n\s]/,'')
   puts my_file.inspect
+  
+  xml_file = open my_file
+  #ini adalah instance variable
+  @pilihan = pilihan
+  
   #myfile = 'resume.xml'
-  doc = Nokogiri::XML File.open(my_file)
-  doc.search('item').each do |item|
-    puts "===================================================================="
-    puts "Title: #{item.at('title').text}"
-    puts "Date: #{item.at('pubDate').text}"
-    puts "Url: #{item.at('link').text}"
-    puts "===================================================================="
-    puts " \n"
-    puts " \n"
-  end
-end
-
-#method baca rss
-def baca_rss
-  puts "masukkan url dari rss feed : "
-  my_url = gets.gsub(/[\n\s]/,'')
-  puts my_url.inspect
-
   #contoh url rss feed
   #url = 'http://www.ruby-lang.org/en/feeds/news.rss'
-  xml_file = open my_url
-  doc = Nokogiri::XML xml_file
+  
+  if pilihan == 1
+    doc = Nokogiri::XML File.open(my_file)
+  elsif pilihan == 2
+    doc = Nokogiri::XML xml_file
+  end   
+     
   doc.search('item').each do |item|
     puts "===================================================================="
     puts "Title: #{item.at('title').text}"
@@ -41,13 +33,13 @@ def baca_rss
   end
 end
 
-
+#ini adalah menu pilihan aplikasi
 puts
 loop do
   choose do |menu|
     menu.prompt = "Silahkan pilih menu mas Bro !!! "
-    menu.choice(:XML) { baca_xml() }
-    menu.choice(:RSS) { baca_rss() }
+    menu.choice(:XML) { baca_xml(1) }
+    menu.choice(:RSS) { baca_xml(2) }
     menu.choice(:Quit, "Exit program.") { exit }
   end
 end
